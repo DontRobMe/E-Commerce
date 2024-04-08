308af28e7ac8c62c4546546e40c51ef7c916c0dc
francois gourbal <gourbal.francois@gmail.com>
2024-04-08 09:34:14 +0200

    a la cool

---
+++ ECommerce.API/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Busines.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace E_Commerce.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly IAdminService _projectService;
+        
+        public AdminController(ILogger<AdminController> logger,IAdminService projectService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _projectService = projectService ?? throw new ArgumentNullException(nameof(projectService));
+        }
+    }   
+}
---
+++ ECommerce.API/Controllers/ProduitController.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce.Busines.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProduitController : ControllerBase
+    {
+        private readonly ILogger<ProduitController> _logger;
+        private readonly IProduitService _projectService;
+        public ProduitController(ILogger<ProduitController> logger,IProduitService projectService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _projectService = projectService ?? throw new ArgumentNullException(nameof(projectService));
+        }
+    }   
+}
