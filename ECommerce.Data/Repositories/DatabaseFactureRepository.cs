e1f42715bad11fd7fbdd7b1457f902f0eca86090
Gourbal François <francois.gourbal@ynov.com>
2024-05-23 22:32:05 +0200

    ok

--- ECommerce.Data/Repositories/DatabaseFactureRepository.cs
+++ ECommerce.Data/Repositories/DatabaseFactureRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace E_Commerce.Data.Repositories;
+﻿using E_Commerce.Business.IRepositories;
+using E_Commerce.Business.Models;
+using E_Commerce.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
-public class DatabaseFactureRepository
+namespace E_Commerce.Data.Repositories;
+
+public class DatabaseFactureRepository : IFactureRepository
 {
+    private readonly MyDbContext _dbContext;
+
+    
+    public DatabaseFactureRepository(MyDbContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+    
+    
+    public async Task<Facture> AddFacture(Facture facture)
+    {
+        await _dbContext.Factures.AddAsync(facture);
+        await _dbContext.SaveChangesAsync();
+        return facture;
+    }
+    
+    public async Task<IEnumerable<Facture>> GetFactures()
+    {
+        return await _dbContext.Factures.ToListAsync();
+    }
     
 }
